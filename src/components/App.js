847c2f202a666055428f24fb74a7601f65561d3f
E Luckie <eluckie.d@gmail.com>
2023-04-16 16:57:00 -0600

    set up basic project structure

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Browse from "./Browse";
+import Rentals from "./Rentals";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <Browse />
+      <Rentals />
+    </React.Fragment>
+  );
+}
+
+export default App;
